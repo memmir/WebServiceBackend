227d225c3d5b776cb17c2b2d2d19415450f9b199
Mahmut Emir Kilinc <memir41@gmail.com>
2023-09-26 22:31:05 +0300

    Entity and Dto classes are created.

---
+++ src/main/java/com/example/WebService/dto/EmployeeDto.java
@@ -0,0 +1,12 @@
+package com.example.WebService.dto;
+
+public class EmployeeDto {
+
+    String id;
+    String name;
+    String surname;
+    String address;
+    int salary;
+    String department;
+    boolean remote;
+}
--- src/main/java/com/example/WebService/entity/Employee.java
+++ src/main/java/com/example/WebService/entity/Employee.java
@@ -1,9 +1,33 @@
-/*
+
 package com.example.WebService.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
 @Entity
 public class Employee {
+
+    @Id
+    @Column
+    String id;
+
+    @Column
+    String name;
+
+    @Column
+    String surname;
+
+    @Column
+    String address;
+
+    @Column
+    int salary;
+
+    @Column
+    String department;
+
+    @Column
+    boolean remote;
 }
-*/
+
