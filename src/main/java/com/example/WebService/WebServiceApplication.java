1b8a31c68402fafbd6a4868b78e99d73ff09976d
Mahmut Emir Kilinc <memir41@gmail.com>
2023-09-26 18:55:17 +0300

    Added some configurations. Built it up the application succesfully.

--- src/main/java/com/example/WebService/WebServiceApplication.java
+++ src/main/java/com/example/WebService/WebServiceApplication.java
@@ -2,8 +2,8 @@ package com.example.WebService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class WebServiceApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/example/WebService/entity/Employee.java
@@ -0,0 +1,9 @@
+/*
+package com.example.WebService.entity;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Employee {
+}
+*/
